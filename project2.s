e7ea5766902a64f09d7a7ecca9e27b213ce19305
Chadille <Chadille@ENG-326829.labs.cldc.howard.edu>
2019-11-13 19:34:20 -0500

    Initializing variables

--- project2.s
+++ project2.s
@@ -16,9 +16,12 @@ main:
 	li $t1, 4  #initialized the variable to check if number of characters is > 4
 	li $t2, 32 #loaded a space here 
 	li $t3, 9 #loaded a tab here
+	li $t5, 0 #initialized count of valid characters
 	
 	loop: 
-		
+		bgt $t5, $t1, print_invalid_output	#if number of valid characters is greater than 4 then print invalid output	
 		lb $t4, 0($t0)  #gets a character of the string
+		
+	print_invalid_output #
 	
 	
