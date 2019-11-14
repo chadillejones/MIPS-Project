ffe9600e3e1c6fd4e27428098fd8489243e94bdb
Chadille <Chadille@ENG-326829.labs.cldc.howard.edu>
2019-11-14 14:04:45 -0500

    Fixed an error with accepting the user data

--- project2.s
+++ project2.s
@@ -6,11 +6,13 @@ user_input: .space 2000
 invalid_input: .asciiz "Invalid Input"
 
 .text  #Assembly langugae instruction
+.globl main
 
 main:
 
 	li $v0, 8  #accepts user input
 	la $a0, user_input
+	li $a1, 1001 #specify the length of the input #changed so the person can enter 10 characters and then enter button
 	syscall
 	
 	la $t0, user_input   #loads the address of the string
