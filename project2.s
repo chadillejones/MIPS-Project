67bb02fc93321c13489fb2867a9101e89d4b1c29
Chadille <Chadille@ENG-326829.labs.cldc.howard.edu>
2019-11-13 19:50:09 -0500

    Creating invalid output function

--- project2.s
+++ project2.s
@@ -16,8 +16,8 @@ main:
 	li $t1, 4  #initialized the variable to check if number of characters is > 4
 	li $t2, 32 #loaded a space here 
 	li $t3, 9 #loaded a tab here
-	li $t5, 0 #initialized count of valid characters
-	li $t6, 0 #initialized count of
+	li $t5, 0 #initialized count of valid characters #delete $t6 as $t5 can be used for its purpose
+	
 	
 	loop: 
 		bgt $t5, $t1, print_invalid_output	#if number of valid characters is greater than 4 then print invalid output	
